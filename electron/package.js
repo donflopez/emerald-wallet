22f14bc071b8b2f9ae625c982592dc13d1b5c1a7
Igor Artamonov <igor@artamonov.ru>
2017-06-22 22:41:13 -0400

    solution: code formatting

--- electron/package.js
+++ electron/package.js
@@ -11,45 +11,45 @@ const argv = require('minimist')(process.argv.slice(2));
  * Configuration for electron-packager
  */
 const packOpts = {
-  dir: './',
-  name: appName,
-  overwrite: true, // overwrite release folder
-  ignore: [
-    '^/release($|/)',
-    '^/.idea($|/)',
-    '^/node_modules($|/)',
-  ]
+    dir: './',
+    name: appName,
+    overwrite: true, // overwrite release folder
+    ignore: [
+        '^/release($|/)',
+        '^/.idea($|/)',
+        '^/node_modules($|/)',
+    ]
 };
 
 // Run webpack to compile main.js
 webpack(webpackConfig, (err, stats) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+    if (err) {
+        console.error(err);
+        return;
+    }
 
-  const platforms = (argv.all ? ['linux', 'win32'] : [os.platform()]);
-  // Run electron-packager for each platform
-  platforms.forEach(function(platform) {
-    pack(platform, os.arch());
-  })
+    const platforms = (argv.all ? ['linux', 'win32'] : [os.platform()]);
+    // Run electron-packager for each platform
+    platforms.forEach(function (platform) {
+        pack(platform, os.arch());
+    })
 });
 
 
 function pack(platform, arch) {
-  console.log(`Start packaging for ${platform}`);
-  const opts = Object.assign({}, packOpts, {
-    platform: platform,
-    arch: arch,
-    out: `release`
-  });
+    console.log(`Start packaging for ${platform}`);
+    const opts = Object.assign({}, packOpts, {
+        platform: platform,
+        arch: arch,
+        out: `release`
+    });
 
-  packager(opts, onPackFinished);
+    packager(opts, onPackFinished);
 }
 
 function onPackFinished(err, appPaths) {
-  if (err) {
-    return console.error(err);
-  }
-  console.log(`Finished ${appPaths}`)
+    if (err) {
+        return console.error(err);
+    }
+    console.log(`Finished ${appPaths}`)
 }
